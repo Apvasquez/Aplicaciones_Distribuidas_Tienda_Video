50a1d6026d5a621f9def5382a6fc6dd2f158a299
Alex Paul <alex_12paul@hotmail.es>
2022-01-07 12:12:10 -0500

    Login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-sans text-gray-900 bg-gradient-to-r from-[#2A475E] to-[#1B2838] antialiased">
             {{ $slot }}
         </div>
     </body>
