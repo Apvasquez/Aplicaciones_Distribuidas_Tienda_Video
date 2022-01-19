e433380ab708fb1addd0e67225b439605c13cbba
Alex Paul <alex_12paul@hotmail.es>
2022-01-19 10:09:45 -0500

    Subida

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
     <div class="my-0">
 
-        <div class="max-w-7xl mx-auto ">
+        <div class="max-w-5xl mx-auto ">
             <div class="bg-[#1B2838] overflow-hidden shadow-xl sm:rounded-lg">
                 <x-carrousel/>
                 <x-footer_1/>
